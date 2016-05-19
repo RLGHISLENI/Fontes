8c1f96ed10b502a924d572469beba3045ce2f1e6
RLGHISLENI <beto.ghisleni@gmail.com>
2016-05-19 08:35:19 -0300

    Add #Region

--- source/Modelo.EcoSistema/rlg.Domain/Models/User.cs
+++ source/Modelo.EcoSistema/rlg.Domain/Models/User.cs
@@ -6,29 +6,42 @@ namespace rlg.Domain.Models
   public class User
   {
 
-    #region "  Construtor "
+    #region Construtor
+
     protected User() { }
+
     public User(string name, string email)
     {
       this.Id = Guid.NewGuid();
       this.Name = name;
       this.Email = email;
     }
+
     #endregion
- 
+
+    #region Propriedades
 
     public Guid Id { get; private set; }
+
     public string Name { get; private set; }
+
     public string Email { get; private set; }
+
     public string Password { get; private set; }
+
+    #endregion
+
+    #region Métodos
 
     public void setPassword(string password, string confirmPassword)
     {
       AssertionConcern.AssertArgumentNotNull(password, "Senha inválida");
       AssertionConcern.AssertArgumentNotNull(confirmPassword, "Confirmação de Senha inválida");
       AssertionConcern.AssertArgumentEquals(password, confirmPassword, "Senha diferente da confirmação");
-      AssertionConcern.AssertArgumentLength(password,6,20, "Senha inválida");
+      AssertionConcern.AssertArgumentLength(password, 6, 20, "Senha inválida");
     }
+
+    #endregion
 
   }
 }
