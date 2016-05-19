e8987a3f9ad689b274a38f881aa4faf02d195af2
RLGHISLENI <beto.ghisleni@gmail.com>
2016-05-19 13:54:47 -0300

    Add ConsoleApp

---
+++ source/Modelo.EcoSistema/rlg.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace rlg.ConsoleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Bem Vindo!");
+      Console.ReadKey();
+    }
+  }
+}
