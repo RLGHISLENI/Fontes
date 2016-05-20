cb53c096f29c10542ed0de0ce35c1ed867234207
RLGHISLENI <beto.ghisleni@gmail.com>
2016-05-20 13:40:55 -0300

    Utilização de using para simplificação do code

--- source/Modelo.EcoSistema/rlg.Common/Validation/PasswordAssertionConcern.cs
+++ source/Modelo.EcoSistema/rlg.Common/Validation/PasswordAssertionConcern.cs
@@ -1,18 +1,21 @@
-﻿namespace rlg.Common.Validation
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace rlg.Common.Validation
 {
     public class PasswordAssertionConcern
     {
         public static void AssertIsValid(string password)
         {
-            AssertionConcern.AssertArgumentNotNull(password, "Senha do usuário inválida");
+            AssertionConcern.AssertArgumentNotNull(password, "Senha inválida");
         }
 
         public static string Encrypt(string password)
         {
             password += "|2d331cca-f6c0-40c0-bb43-6e32989c2881";
-            System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
-            byte[] data = md5.ComputeHash(System.Text.Encoding.Default.GetBytes(password));
-            System.Text.StringBuilder sbString = new System.Text.StringBuilder();
+            MD5 md5 = MD5.Create();
+            byte[] data = md5.ComputeHash(Encoding.Default.GetBytes(password));
+            StringBuilder sbString = new StringBuilder();
             for (int i = 0; i < data.Length; i++)
                 sbString.Append(data[i].ToString("x2"));
             return sbString.ToString();
