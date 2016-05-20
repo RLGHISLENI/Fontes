b9bc74d534c486b7342cd8ed8190e2250d091cb4
RLGHISLENI <beto.ghisleni@gmail.com>
2016-05-20 12:19:38 -0300

    Definição do fluxo de execução

--- source/Modelo.EcoSistema/rlg.ConsoleApp/Program.cs
+++ source/Modelo.EcoSistema/rlg.ConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Windows;
 using rlg.Domain.Models;
-
 
 namespace rlg.ConsoleApp
 {
@@ -11,22 +9,34 @@ namespace rlg.ConsoleApp
     {
       try
       {
-        User usuario = new User("Robertinho", "beto.ghisleni@gmail.com");
-        usuario.setPassword("1234567890", "1234567890");
+        // Cria usuário e define senha
+        Usuario usuario = new Usuario("Robertinho", "beto.ghisleni@gmail.com");
+        usuario.definirSenha("1234567890", "1234567890");
 
-        Console.WriteLine("Bem Vindo " + usuario.Name + "!");
+        // Boas vindas e demonstraçã das informações do usuário
+        Console.WriteLine("Bem Vindo " + usuario.Nome + "!");
         Console.WriteLine("Id: " + usuario.Id );
         Console.WriteLine("Email: " + usuario.Email);
-        Console.WriteLine("Senha: " + usuario.Password);
+        Console.WriteLine("Senha: " + usuario.Senha);
+        usuario.Validar();
+        Console.WriteLine("Usuário validado com sucesso!");
         Console.ReadKey();
-        
+
+        // Troca de nome e redefinição de senha
+        usuario.AlterarNome("Beto Ghisleni");
+        usuario.redefinirSenha();
+        Console.WriteLine("Novo Nome: " + usuario.Nome);
+        Console.WriteLine("Senha Redefinida: " + usuario.Senha);
+        usuario.Validar();
+        Console.WriteLine("Redefinição do usuário validado com sucesso!");
+        Console.ReadKey();
+
       }
       catch (Exception ex)
       {
         Console.WriteLine(ex.Message);
         Console.ReadKey();
-      }
-      
+      }      
     }
   }
 }
