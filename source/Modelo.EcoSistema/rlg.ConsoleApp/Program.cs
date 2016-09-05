0f553f78c921f4b965b2d37950ce80ef6bd57232
RLGHISLENI <beto.ghisleni@gmail.com>
2016-09-05 14:16:11 -0300

    Adequação aos novos métodos

--- source/Modelo.EcoSistema/rlg.ConsoleApp/Program.cs
+++ source/Modelo.EcoSistema/rlg.ConsoleApp/Program.cs
@@ -20,37 +20,29 @@ namespace rlg.ConsoleApp
                     Console.Write("Email: ");
                     string email = Console.ReadLine();
 
-                    // Cria usuário 
-                    Usuario usuario = new Usuario(nome, email);
-                    usuario.definirNomeUsuarioScopesEhValido();
-                    usuario.definirEmailUsuarioScopesEhValido();
-
                     Console.Write("Senha: ");
                     string senha = Console.ReadLine();
-                    //usuario.definirSenhaUsuarioScopesEhValido();
 
                     Console.Write("Confirmação: ");
                     string confirmaSenha = Console.ReadLine();
-                    //usuario.definirSenhaUsuarioScopesEhValido(confirmaSenha);
 
-                    // Define senha
-                    usuario.definirSenha(senha, confirmaSenha);
+                    // Cria usuário e valida todos os atributos
+                    Usuario usuario = new Usuario(nome, email, senha, confirmaSenha);
 
                     // Boas vindas e demonstração das informações do usuário
                     Console.WriteLine("Bem Vindo {0}{1}", usuario.Nome, "!");
                     Console.WriteLine("Id: {0}", usuario.Id);
                     Console.WriteLine("Email: {0}", usuario.Email);
                     Console.WriteLine("Senha: {0}", usuario.Senha);
-                    usuario.ehValido();
+                    //usuario.ehValido();
                     Console.WriteLine("Usuário validado com sucesso!");
                     Console.ReadKey();
 
                     // Troca de nome e redefinição de senha
                     Console.Write("Novo Nome: ");
                     usuario.definirNome(Console.ReadLine());
-                    usuario.definirNomeUsuarioScopesEhValido();
                     usuario.redefinirSenha();
-                    usuarioValido = usuario.ehValido();
+                    //usuarioValido = usuario.ehValido();
 
                     Console.WriteLine("Olá {0}", usuario.Nome);
                     Console.WriteLine("Senha Redefinida: {0}", usuario.Senha);
